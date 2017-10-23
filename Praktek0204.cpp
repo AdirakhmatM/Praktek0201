40092b793a222c160227ea8592175c87aef2b7e8
AdirakhmatM <32364278+AdirakhmatM@users.noreply.github.com>
2017-10-23 19:25:30 +0800

    Output Nilai

--- Praktek0204.cpp
+++ Praktek0204.cpp
@@ -7,4 +7,8 @@ int main(){
 	double Luas_1box = Lantai_parket*8/1000000;	
 	double Jumlah_boxpkt, Biaya_pkt, Biaya_pg, TotalB;
 	
+	cout <<" Luas Rumah 			: "<<Luas_rumah<<" m2"<<endl;
+	cout <<" Luas 1 Box Parket 		: "<<Luas_1box<<" m2"<<endl;
+	
+	
 }
